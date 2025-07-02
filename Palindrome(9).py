4914f94adc232d2764ed8812bf8b83414f6ede70
Kannoujiya Ku Soni Ramesh <152206385+Sonu281004@users.noreply.github.com>
2025-07-02 14:17:55 +0530

    Add files via upload

---
+++ Palindrome(9).py
@@ -0,0 +1,8 @@
+class solution:
+    def isPalindeome(self, x:int) -> bool:
+        s = str(x)
+        rev = s[::-1]
+        if (s==rev):
+            return True
+        else:
+            return False
